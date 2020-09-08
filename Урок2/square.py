99bfc866d87c3f6ec41fff6cb0717b1bfba9cc85
rs.silin <rs.silin@tensor.ru>
2020-09-08 18:05:45 +0300

    rfix-lab

--- Урок2/square.py
+++ Урок2/square.py
@@ -1,10 +1,7 @@
 import math
-#y = ax^2+bx+c
-#D = b^2 -4ac
-#D >=0
-'''
-проверяем строку на число
-'''
+
+#проверяем строку на число
+
 def is_digit(string):
     if string.isdigit():
        return False
@@ -14,34 +11,34 @@ def is_digit(string):
             return False
         except ValueError:
             return True
-
+#убеждаемся, что ввели число
 test_a=True
 test_b=True
 test_c=True
 while test_a==True:
-    a = int(input ("Input a: "))
+    a = float(input ("Input a: "))
     test_a=is_digit(str(a))
     continue
 while test_b==True:
-    b = int(input ("Input b: "))
+    b = float(input ("Input b: "))
     test_b=is_digit(str(b))
     continue
 while test_c==True:
-    c = int(input ("Input c: "))
+    c = float(input ("Input c: "))
     test_c=is_digit(str(c))
     continue
-if a != 0:
+if a != 0: #проверяем, чтобы в знаменателях не было нулей
     D = b**2 - 4*a*c
     if D >=0:
-        x1 = -b + math.sqrt(D)/(2*a)
-        x2 = -b - math.sqrt(D)/(2*a)
-        print (x1,x2)
+        x1 = (-b + math.sqrt(D))/(2*a)
+        x2 = (-b - math.sqrt(D))/(2*a)
+        print ("x1={:.2f}, x2={:.2f}".format(x1,x2))
     else:
         print("there is no solutions!")
 else:
     if b!=0:
         x=-c/b
-        print("x={}".format(x))
+        print("x={:.2f}".format(x))
     else:
         if c!=0:
             print("there is no solutions")
