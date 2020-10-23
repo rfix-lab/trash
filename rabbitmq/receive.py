9287da109dd381959f5fdfa2ea5844532a627754
rs.silin <rs.silin@tensor.ru>
2020-10-23 18:15:40 +0300

    ok

--- rabbitmq/receive.py
+++ rabbitmq/receive.py
@@ -5,15 +5,15 @@ def callback(ch, method, properties, body):
 
 def main():
     credentials = pika.PlainCredentials('mqadmin', 'mqadmin')
-    parameters = pika.ConnectionParameters('rabbitmq-lb', 5672, '/', credentials)
+    parameters = pika.ConnectionParameters('10.176.15.27', 5672, '/', credentials)
     connection = pika.BlockingConnection(parameters)
 
     channel = connection.channel()
 
-    channel.queue_declare(queue='queue1')
+    channel.queue_declare(queue='PSqueue')
 
     channel.basic_consume(
-        queue='queue1', on_message_callback=callback, auto_ack=True)
+        queue='PSqueue', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- rabbitmq/send.py
+++ rabbitmq/send.py
@@ -2,15 +2,15 @@ import pika
 
 def main():
     credentials = pika.PlainCredentials('mqadmin', 'mqadmin')
-    parameters = pika.ConnectionParameters('rabbitmq-lb', 5672, '/', credentials)
+    parameters = pika.ConnectionParameters('10.176.15.27', 5672, '/', credentials)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
-    channel.queue_declare(queue='queue1')
+    channel.queue_declare(queue='PSqueue')
 
-    while True:
-      channel.basic_publish(exchange='', routing_key='queue1', body='Hello World!')
-      print(" [x] Sent 'Hello World!'")
+    #while True:
+    channel.basic_publish(exchange='', routing_key='PSqueue', body='Ya-hooo!')
+    print(" [x] Sent 'Ya-hooo!'")
 
 
     connection.close()
