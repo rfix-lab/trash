801caef4f0cdc37eb79a03ea3d0dd341d1404aec
rs.silin <rs.silin@tensor.ru>
2021-08-10 16:06:51 +0300

    ok

---
+++ scrapping/try.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://quotes.toscrape.com/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('span', class_='text')
+authors = soup.find_all('small', class_='author')
+tags = soup.find_all('div', class_='tags')
+
+#for i in range (0,len(quotes)):
+ #   print (authors[i].text)
+#for i in authors:
+ #   print (i.text)
+for i in tags:
+    print (i.text)
